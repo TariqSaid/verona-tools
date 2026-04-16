125e0d2edfc6da8d8a651a15fd0da6f8666e89bd
Tariq Said <95426791+TariqSaid@users.noreply.github.com>
2026-04-16 23:37:11 +0100

    Update outillage.js

---
+++ public/data/outillage.js
@@ -0,0 +1,48 @@
+'use strict';
+window.VT_PRODUCTS = (window.VT_PRODUCTS || []).concat([
+  {
+    id          : 1776205605844,
+    category    : 'Outillage',
+    subcategory : 'Scies électriques',
+    brand       : 'Bosch',
+    title       : 'Scie Sauteuse Bosch Professional GST 650',
+    description_fr: 'Scie sauteuse professionnelle Bosch GST 650 dotée d\'un moteur puissant de 650W pour découpes précises dans le bois, le métal et les matériaux composites.',
+    price_MAD   : 449,
+    in_stock    : true,
+    ref         : 'VT-AUTO-5844',
+    images      : [
+      '/images/products/bosch-professional-gst-650-jigsaw-1-601721.png',
+      '/images/products/bosch-professional-gst-650-jigsaw-2-601721.png'
+    ],
+  },
+  {
+    id          : 1776206272542,
+    category    : 'Outillage',
+    subcategory : 'Perceuses et Visseuses',
+    brand       : 'Bosch Professional',
+    title       : 'Perceuse-Burin Rotative Bosch Professional GBH 220',
+    description_fr: 'Perceuse-burin rotative professionnelle Bosch GBH 2-26 équipée d\'un mandrin SDS-plus pour le forage et le burinage dans le béton, la maçonnerie et la pierre.',
+    price_MAD   : 1200,
+    in_stock    : true,
+    ref         : 'VT-AUTO-2542',
+    images      : [
+      '/images/products/bosch-professional-gbh-2-26-rotary-hammer-drill-1-268034.png',
+      '/images/products/bosch-professional-gbh-2-26-rotary-hammer-drill-2-268034.png'
+    ],
+  },
+  {
+    id          : 1776207905844,
+    category    : 'Outillage',
+    subcategory : 'Scies électriques',
+    brand       : 'Bosch',
+    title       : 'Meuleuse angulaire PRO GWS 750-115',
+    description_fr: 'Meuleuse angulaire professionnelle Bosch GWS 750-115 avec moteur 750W et disque 115mm pour découpe et meulage.',
+    price_MAD   : 715,
+    in_stock    : true,
+    ref         : 'VT-AUTO-5844',
+    images      : [
+      '/images/products/bosch-gws-750-115-professional-angle-grinder-1-960119.png',
+      '/images/products/bosch-gws-750-115-professional-angle-grinder-2-960119.png'
+    ],
+  },
+]);
